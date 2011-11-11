a804d4ada2ebb76f34d147055fb075c038742d05
=Kamil Hakim <khakim@kth.se>
2011-11-11 10:54:46 +0100

    init commit

---
+++ SLParser.Domain/SL.Domain/RealtimeInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace SLParser.Domain
+{
+    public class RealtimeInfo
+    {
+        public List<Bus> Buses { get; set; }
+    }
+}
---
+++ SLParser.Domain/SL.Utilities/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SL.Utilities
+{
+    public class Class1
+    {
+    }
+}
